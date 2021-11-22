2b329c149c0b340dabeda69398bf8bde096fc400
Diego <dfreile51@gmail.com>
2021-11-22 13:07:26 +0100

    0497.04 Corregido con respecto al anterior

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -7,9 +7,9 @@ public class AireAcondicionado {
     // Minimo de temperatura
     private double min;
     // Maxima temperatura seleccionada
-    private double maxseleccionada;
+    private double maxSeleccionada;
     // Minima temperatura seleccionada
-    private double minseleccionada;
+    private double minSeleccionada;
     // Incremento de temperatura
     private double incremento;
     // Cambios de temperatura
@@ -20,17 +20,19 @@ public class AireAcondicionado {
      */
     public AireAcondicionado(double maximaTemperatura, double minimaTemperatura) {
         temperatura = 15.0;
-        maxseleccionada = maximaTemperatura;
-        minseleccionada = minimaTemperatura;
-        max = 20.0;
-        min = 0.0;
+        maxSeleccionada = temperatura;
+        minSeleccionada = temperatura;
+        max = maximaTemperatura;
+        min = minimaTemperatura;
         incremento = 5.0;
+        cambiosTemperatura = 0;
     }
     
     // Subir la temperatura
     public void subirTemperatura() {
-        if ((temperatura + incremento) <= maxseleccionada) {
+        if ((temperatura + incremento) <= max) {
             temperatura = temperatura + incremento;
+            cambiosTemperatura = cambiosTemperatura + 1;
         }
         else {
             System.out.println("No se puede subir más la temperatura, se ha llegado al máximo");
@@ -39,8 +41,9 @@ public class AireAcondicionado {
     
     // Bajar la temperatura
     public void bajarTemperatura() {
-        if ((temperatura - incremento) >= minseleccionada) {
+        if ((temperatura - incremento) >= min) {
             temperatura = temperatura - incremento;
+            cambiosTemperatura = cambiosTemperatura + 1;
         }
         else {
             System.out.println("No se puede bajar más la temperatura, se ha llegado al mínimo");
@@ -70,7 +73,7 @@ public class AireAcondicionado {
     
     // Devuelve las estadísticas
     public String getEstadisticas() {
-        String estadisticas = ("Temperatura actual: " + temperatura + "- Temp. máxima posible: " + max + "- Temp. mínima posible: " + min + " - Temp. máxima seleccionada: " + maxseleccionada + " - Temp. mínima seleccionada: " + minseleccionada + " - Temperatura cambiada " + cambiosTemperatura + " veces");
+        String estadisticas = ("Temperatura actual: " + temperatura + "- Temp. máxima posible: " + max + "- Temp. mínima posible: " + min + " - Temp. máxima seleccionada: " + maxSeleccionada + " - Temp. mínima seleccionada: " + minSeleccionada + " - Temperatura cambiada " + cambiosTemperatura + " veces");
         return estadisticas;
     }
 }
