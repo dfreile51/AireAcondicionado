19e4cde0c697c1d121526319bdcc5d7f4be4d468
Diego <dfreile51@gmail.com>
2021-11-22 09:28:12 +0100

    0497.04 Creado mostrarEstadisticas y getEstadisticas

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -6,22 +6,30 @@ public class AireAcondicionado {
     private double max;
     // Minimo de temperatura
     private double min;
+    // Maxima temperatura seleccionada
+    private double maxseleccionada;
+    // Minima temperatura seleccionada
+    private double minseleccionada;
     // Incremento de temperatura
     private double incremento;
+    // Cambios de temperatura
+    private int cambiosTemperatura;
 
     /** 
      * Crea un aireacondicionado
      */
     public AireAcondicionado(double maximaTemperatura, double minimaTemperatura) {
         temperatura = 15.0;
-        max = maximaTemperatura;
-        min = minimaTemperatura;
+        maxseleccionada = maximaTemperatura;
+        minseleccionada = minimaTemperatura;
+        max = 20.0;
+        min = 0.0;
         incremento = 5.0;
     }
     
     // Subir la temperatura
     public void subirTemperatura() {
-        if ((temperatura + incremento) <= max) {
+        if ((temperatura + incremento) <= maxseleccionada) {
             temperatura = temperatura + incremento;
         }
         else {
@@ -31,7 +39,7 @@ public class AireAcondicionado {
     
     // Bajar la temperatura
     public void bajarTemperatura() {
-        if ((temperatura - incremento) >= min) {
+        if ((temperatura - incremento) >= minseleccionada) {
             temperatura = temperatura - incremento;
         }
         else {
@@ -52,5 +60,17 @@ public class AireAcondicionado {
         else {
             System.out.println("No se pueden incrementos negativos");
         }
+    }
+    
+    // Muestra por pantalla las estadisticas
+    public void mostrarEstadisticas() {
+        String detalles = getEstadisticas();
+        System.out.println(detalles);
+    }
+    
+    // Devuelve las estadísticas
+    public String getEstadisticas() {
+        String estadisticas = ("Temperatura actual: " + temperatura + "- Temp. máxima posible: " + max + "- Temp. mínima posible: " + min + " - Temp. máxima seleccionada: " + maxseleccionada + " - Temp. mínima seleccionada: " + minseleccionada + " - Temperatura cambiada " + cambiosTemperatura + " veces");
+        return estadisticas;
     }
 }
